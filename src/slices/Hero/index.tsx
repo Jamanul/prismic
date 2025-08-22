b0bd37c5c73fe0b6a26c71a485a6d7081dd96f7d
Jamanul <jk.skb.25@gmail.com>
2025-08-22 23:00:07 +0600

    added hero with prismic

---
+++ src/slices/Hero/index.tsx
@@ -0,0 +1,59 @@
+import { FC } from "react";
+import { Content } from "@prismicio/client";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
+import { Bounded } from "@/components/Bounded";
+import clsx from "clsx";
+
+/**
+ * Props for `Hero`.
+ */
+export type HeroProps = SliceComponentProps<Content.HeroSlice>;
+
+/**
+ * Component for "Hero" Slices.
+ */
+const Hero: FC<HeroProps> = ({ slice }) => {
+  return (
+    <Bounded
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+      className="relative min-h-screen overflow-hidden bg-neutral-950"
+    >
+      <div className="absolute inset-0 top-0 left-0 scale-125">
+        <PrismicNextImage
+          priority
+          fill
+          className="object-cover opacity-50"
+          field={slice.primary.image}
+          alt=""
+        />
+      </div>
+      <div className="relative z-1 flex h-screen flex-col justify-center">
+        <div className="display-font max-w-xl text-6xl leading-tight text-neutral-50 md:text-7xl lg:text-8xl">
+          <PrismicRichText field={slice.primary.heading} />
+        </div>
+        <div className="mt-6 max-w-md text-lg text-neutral-100">
+          <PrismicRichText field={slice.primary.body} />
+        </div>
+        <div className="mt-8">
+          {slice.primary.button.map((link) => (
+            <PrismicNextLink
+              key={link.key}
+              field={link}
+              className={clsx(
+                "inline-flex items-center justify-center px-12 py-4 text-center font-extrabold tracking-wider uppercase transition-colors duration-300",
+                link.variant === "Secondary"
+                  ? "border border-white text-white hover:bg-white/20"
+                  : "bg-white text-black hover:bg-white/80",
+                "w-fit",
+              )}
+            />
+          ))}
+        </div>
+      </div>
+    </Bounded>
+  );
+};
+
+export default Hero;
